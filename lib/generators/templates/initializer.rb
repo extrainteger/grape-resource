6d20e153f93e47390dd926a0b0c97e95bb837bd2
Ashar Mubasir <asharmubasir@Ashars-MacBook-Pro.local>
2019-07-05 14:53:43 +0700

    Add config for success/error formatter

--- lib/generators/templates/initializer.rb
+++ lib/generators/templates/initializer.rb
@@ -7,4 +7,8 @@ GrapeResource.setup do |config|
 
   # Entry point routes
   config.entry_point_routes = "# Routes entry point"
+
+  # Formatter
+  config.success_formatter = "formatter :json, ::API::SuccessFormatter"
+  config.error_formatter = "error_formatter :json, ::API::ErrorFormatter"
 end
